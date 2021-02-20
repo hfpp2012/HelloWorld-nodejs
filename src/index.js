ccaf47cf597fb303846d34a6e3a10dc79738913b
hfpp2012 <hfpp2012@gmail.com>
2021-02-20 19:52:46 +0800

    change

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
+"use strict";
 
-'use strict'
-
-module.exports.handler = function(req, resp, context) {
-    console.log('hello world');
-    resp.setHeader('Content-type', 'text/plain');
-    resp.send('Hello world!');
-}
+module.exports.handler = function (req, resp, context) {
+  console.log("hello world");
+  resp.setHeader("Content-type", "text/plain");
+  resp.send("Hello qiuzhi99!");
+};
